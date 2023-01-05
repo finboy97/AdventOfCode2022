703078b6def8c7786c0e1e2bd56d470783ef36c9
finboy97 <finbar201@gmail.com>
2023-01-05 14:33:41 +0000

    Solution for day 3-1

--- AoC2022/src/main/java/day3/Rucksack.java
+++ AoC2022/src/main/java/day3/Rucksack.java
@@ -4,27 +4,51 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 
 public class Rucksack {
 
+    String repeats = new String();
+
+    Integer score = 0; 
 
     public void readLines(){
         try {
             String line;
             
-            BufferedReader reader = new BufferedReader(new FileReader("/home/finbar/Documents/advent_of_code_2022/AdventOfCode2022/AoC2022/src/main/java/day3/test.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("/home/finbar/Documents/advent_of_code_2022/AdventOfCode2022/AoC2022/src/main/java/day3/input.txt"));
+            
+            
             
             while((line = reader.readLine()) != null) {
+                HashSet<Character> pocket1Chars = new HashSet<>();
+                HashSet<Character> duplicates = new HashSet<>();
                 int lineLength = line.length();
                 String pocket1 = line.substring(0, (lineLength/2));
-                String pocket2 = line.substring(lineLength/2);
-                if(line.length() != 0){
-                    System.out.println(pocket1);
-                    System.out.println(pocket2);
-                    
-                }else {
-                    
+                for (int i = 0; i< pocket1.length(); i++){
+                    pocket1Chars.add(pocket1.charAt(i));
                 }
+                String pocket2 = line.substring(lineLength/2);
+
+                for (int i = 0; i<pocket2.length(); i++){
+                    if (pocket1Chars.contains(pocket2.charAt(i))){
+                        System.out.println(Character.toString(pocket2.charAt(i)));
+                        if (duplicates.contains(pocket2.charAt(i))){
+                            continue;
+                        } else {
+                            duplicates.add(pocket2.charAt(i));
+                            repeats = repeats + pocket2.charAt(i);
+                            if (Character.isLowerCase(pocket2.charAt(i))){
+                                score += ( (int) pocket2.charAt(i) )-96;
+                            }
+                            else {
+                                score += (int) pocket2.charAt(i)-38;
+                            }
+                        }
+                        
+                    }
+                }
+                
             }
             reader.close();
             
@@ -33,7 +57,8 @@ public class Rucksack {
         } catch (IOException IOerror) {
             System.out.println("IO Exception");
         }
-        
+        System.out.println(this.repeats);
+        System.out.println(Integer.toString(score));
      
 }
 
