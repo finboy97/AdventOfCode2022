e14a03e6085cc3f22425f19f30af76ca41bfe6ae
finboy97 <finbar201@gmail.com>
2023-01-13 09:56:07 +0000

    More work on day 5 - A

--- AoC2022/src/main/java/day05/SupplyStacks.java
+++ AoC2022/src/main/java/day05/SupplyStacks.java
@@ -24,23 +24,21 @@ public class SupplyStacks {
             if (line.length()==0){
 
                 break;
-            }
-            parseLineIntoStack(line);
-            //System.out.println(line);
-            //System.out.println(line.charAt(1));
+            }           
         }
-        System.out.println(stackSequence.length);
-        for (int i = 0; i< stackSequence.length; i++){
-            System.out.print(Character.toString(stackSequence[i].pop()));
-        }        
+        while ((line = br.readLine())!= null){
+            carryOutInstruction(parseInstruction(line));
+        }
         br.close();
+        for (int i = 0; i < stackSequence.length; i++){
+            System.out.print(Character.toString(stackSequence[i].peek()));
+        }
     }
 
     public void parseLineIntoStack(String line){
         for (int i = 1; i< line.length(); i+=4){
             if (Character.isLetter(line.charAt(i))){
                 if (i == 1){
-                    System.out.println("Stack 0: Value "+ Character.toString(line.charAt(i)));
                     stackSequence[0].push(line.charAt(i));
                 }else{
                     
@@ -50,6 +48,38 @@ public class SupplyStacks {
             }
         }
     }
+
+    /**
+     * Method for parsing an instruction line.
+     * @param line
+     */
+    public int[] parseInstruction(String line){
+        String[] lineComponents = line.split(" ");
+        int amount = Integer.parseInt(lineComponents[1]);
+        int from = Integer.parseInt(lineComponents[3]);
+        int to = Integer.parseInt(lineComponents[5]);
+        int[] instructions = {amount, from, to};
+        return instructions;
+    }
+
+    /**
+     * Method which handles carrying out an instruction line for crates.
+     * 
+     * @param instructions - Size 3 array of int's. First value is the number of moves, 2nd value is 'from', 3rd value is 'to'.
+     */
+    public void carryOutInstruction(int[] instructions){
+        System.out.println("Pushing " + Integer.toString(instructions[0]) + "crates from" + Integer.toString(instructions[1]) + " into " + Integer.toString(instructions[2]));
+        LinkedList from = stackSequence[instructions[1]];
+        LinkedList to = stackSequence[instructions[2]];
+        int moves = instructions[0];
+
+        for (int i = 0; i< moves; i++){
+            to.push(from.pop());
+        }
+
+    }
+
+
 
     public static void main (String[] args) throws IOException{
         SupplyStacks test = new SupplyStacks();
